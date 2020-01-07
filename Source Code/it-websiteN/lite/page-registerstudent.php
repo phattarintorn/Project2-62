a09b0b043198d6aaf6ea7e78bab04a5f98e53896
Fong Fong <kladpibanfong@gmail.com>
2020-01-07 19:08:56 +0700

    ลบ br

--- Source Code/it-websiteN/lite/page-registerstudent.php
+++ Source Code/it-websiteN/lite/page-registerstudent.php
@@ -88,7 +88,6 @@
                       </button>
                     </div>
                     <br>
-                    <br>
                     <div class="input-group">
                       <span class="input-group-addon"><i class="material-icons" title="ชื่ออาจารย์ที่ปรึกษา">perm_contact_calendar</i></span>
                       <select id="advisor" name="advisor" required class="form-control"  size="">
